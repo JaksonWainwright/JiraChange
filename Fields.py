c8b5e80ba479a208eba0360e21af98e43a1a532f
Jakson Wainwright <jwainwright@GPAdmins-MacBook-Pro.local>
2020-05-06 14:34:03 -0600

    test

--- Fields.py
+++ Fields.py
@@ -9,21 +9,15 @@ class Fields:
         self.jira_comment = Outbound_Webhook.NewOutboundWebhook(self.json_payload)
         self.issue_type = str(self.json_payload['fields']['issuetype']['name'])
 
-<<<<<<< HEAD
-    def parse_ip_customfield(self):
-=======
+
+
     def parse_ip_customfields(self):
->>>>>>> 6a9ec20031b12b73c457c58b9041e9c1c9174fbc
         ip_addresses = str(self.json_payload['fields']['customfield_10065'])
         ip_data = ip_addresses.replace(" ", "/").replace("\r", "").replace("//", "/").split('\n')
         return ip_data
 
     def validate_ip_network(self):
-<<<<<<< HEAD
-        for ip_address in self.parse_ip_customfield():
-=======
         for ip_address in self.parse_ip_customfields():
->>>>>>> 6a9ec20031b12b73c457c58b9041e9c1c9174fbc
             try:
                 ip_network_check = ipaddress.ip_network(ip_address)
             except Exception as errmsg:
@@ -32,11 +26,7 @@ class Fields:
         return conf.validation_success
 
     def validate_ip_global(self):
-<<<<<<< HEAD
-        for ip_address in self.parse_ip_customfield():
-=======
         for ip_address in self.parse_ip_customfields():
->>>>>>> 6a9ec20031b12b73c457c58b9041e9c1c9174fbc
             try:
                 ip_network_check = ipaddress.ip_network(ip_address)
                 ip_is_global = ip_network_check.is_global
@@ -64,7 +54,7 @@ class Fields:
         Outbound_Webhook.send_splunk_notice(f"Field validation started on ticket number: {self.json_payload['key']}")
         customfield_validation_mapper = {
             'IP-Whitelist': self.validate_ip_grammar()
-        }
+
         self.validation_results.append(customfield_validation_mapper.get(self.issue_type))
         print(self.validation_results)
         return self.validation_results
